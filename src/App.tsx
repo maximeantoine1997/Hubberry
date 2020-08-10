92b222f13241777b93efa10608ecfcb8a5146b6b
Maxime Antoine <maximeantoine1997@gmail.com>
2020-08-10 13:53:40 +0200

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,44 @@
-import React from "react";
-import logo from "./logo.svg";
+import { AnimatePresence } from "framer-motion";
+import React, { useEffect } from "react";
+import { Route, Switch, useHistory, useLocation } from "react-router-dom";
+import socketIo from "socket.io-client";
 import "./App.css";
+import Applications from "./pages/Applications";
+import Home from "./pages/Home";
 
-function App() {
+const ENDPOINT = "http://localhost:5000/";
+
+const App = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    console.log("listening...");
+    const socket = socketIo(ENDPOINT, {
+      transports: ["websocket"],
+    });
+
+    socket.on("open", (response: any) => {
+      const path = response === "home" ? "/" : `/${response}`;
+      history.push(path);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Welcome to Hubberry
-      </header>
-    </div>
+    <AnimatePresence exitBeforeEnter>
+      <Switch location={location} key={location.pathname}>
+        <Route path="/apps">
+          <Applications />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </AnimatePresence>
   );
-}
+};
 
 export default App;
