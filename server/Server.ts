cb0c398e967c56e1b118f364fd58bc5072a61374
Maxime Antoine <maximeantoine1997@gmail.com>
2020-08-10 13:53:21 +0200

    Add sockets

--- server/Server.ts
+++ server/Server.ts
@@ -1,14 +1,24 @@
-import bodyParser from "body-parser";
-import express from "express";
+import express, { json } from "express";
+import http from "http";
+import socket from "socket.io";
 import { dialogflowWebhooks } from "./routes/Dialogflow";
 import { postMessages, putMessage } from "./routes/Messages";
 import { getUser } from "./routes/Users";
 
 const app = express();
+
+const server = http.createServer(app);
+const io = socket.listen(server);
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Socket IO
+io.on("connect", (socket) => {
+  console.log("New user online", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("A user went offline", socket.id);
+  });
+});
 
 // user
 app.get("/api/users/:id", getUser);
@@ -18,6 +28,9 @@ app.post("/api/messages", postMessages);
 app.put("/api/messages/:id", putMessage);
 
 // dialogflow
-app.post("/dialogflow", dialogflowWebhooks);
+app.post("/dialogflow", json(), dialogflowWebhooks);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// listen
+server.listen(port, () => console.log(`Listening on port ${port}`));
+
+export default io;
