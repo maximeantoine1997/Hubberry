cfae6463c7afc86b3d5645c5af6fda918a80907a
Maxime Antoine <maximeantoine1997@gmail.com>
2020-08-10 13:53:30 +0200

    Add sockets

--- server/routes/Dialogflow.ts
+++ server/routes/Dialogflow.ts
@@ -1,19 +1,22 @@
 import { WebhookClient } from "dialogflow-fulfillment-ts";
+import io from "../Server";
 
 export const dialogflowWebhooks = (req: any, res: any) => {
   const agent = new WebhookClient({ request: req, response: res });
 
-  function welcome() {
-    agent.add("Welcome Emma!");
-    agent.add("What can I do for you today?");
+  const sockets = io.sockets;
+  function showFulfillment() {
+    agent.add("Ok, I will show the fulfillment!");
+    sockets.emit("showFulfillment", "DialogFlow called Welcome");
   }
 
   const open = () => {
     agent.add(`For sure I will open ${agent.parameters["App"]}`);
+    sockets.emit("open", agent.parameters["App"]);
   };
 
   let intentMap = new Map();
-  intentMap.set("show fulfillment", welcome);
+  intentMap.set("show fulfillment", showFulfillment);
   intentMap.set("Open", open);
   agent.handleRequest(intentMap);
 };
